15ed7cd60b8e268c607f4e09947a6fcf69ab0a0c
Luis Gil Florian <luisfrancisco17msn@gmail.com>
2025-04-18 21:35:06 -0500

    Creación de Projects-App

---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr class="hover:bg-base-300">
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <InputModal
+    :open="modalOpen"
+    @close="modalOpen = false"
+    @value="onNewValue"
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo Proyecto"
+    subtitle="Dale un nombre único a tu proyecto"
+  />
+  <CustomModal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Titulo del modal</h1>
+    </template>
+    <template #body>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, mollitia blanditiis!
+        Quaerat consequatur deserunt, iure accusamus maxime ipsam.
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex justify-end">
+        <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+        <button @click="customModalOpen = false" class="btn btn-primary">Aceptar</button>
+      </div>
+    </template>
+  </CustomModal>
+
+  <FabButton @click="modalOpen = true">
+    <AddCircle />
+  </FabButton>
+
+  <FabButton @click="customModalOpen = true" position="bottom-left">
+    <ModalIcon />
+  </FabButton>
+</template>
+
+<script setup lang="ts">
+import CustomModal from '@/modules/common/components/CustomModal.vue';
+import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import ModalIcon from '@/modules/common/icons/ModalIcon.vue';
+
+import { ref } from 'vue';
+
+const modalOpen = ref(false);
+const customModalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log(projectName);
+};
+</script>
