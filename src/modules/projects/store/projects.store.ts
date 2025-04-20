9a5c2326f6a5c5b1c4a381b8acf38ba2a2228a25
Luis Gil Florian <luisfrancisco17msn@gmail.com>
2025-04-19 20:58:20 -0500

    Instalación y uso de vueuse para LocalStorage

--- src/modules/projects/store/projects.store.ts
+++ src/modules/projects/store/projects.store.ts
@@ -2,24 +2,25 @@ import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import type { Project } from '../interfaces/project.interface';
 import { v4 as uuidv4 } from 'uuid';
+import { useLocalStorage } from '@vueuse/core';
 
-const initialLoad = (): Project[] => {
-  return [
-    {
-      id: uuidv4(),
-      name: 'Project 1',
-      tasks: [],
-    },
-    {
-      id: uuidv4(),
-      name: 'Project 2',
-      tasks: [],
-    },
-  ];
-};
+// const initialLoad = (): Project[] => {
+//   return [
+//     {
+//       id: uuidv4(),
+//       name: 'Project 1',
+//       tasks: [],
+//     },
+//     {
+//       id: uuidv4(),
+//       name: 'Project 2',
+//       tasks: [],
+//     },
+//   ];
+// };
 
 export const useProjectsStore = defineStore('projects', () => {
-  const projects = ref<Project[]>(initialLoad());
+  const projects = ref(useLocalStorage<Project[]>('projects', []));
 
   const addProject = (name: string) => {
     if (name.length === 0) return;
