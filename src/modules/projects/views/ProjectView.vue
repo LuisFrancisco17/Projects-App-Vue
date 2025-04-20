73debe9f39663592d4bd6bb1f1c5e50a3e91f8c9
Luis Gil Florian <luisfrancisco17msn@gmail.com>
2025-04-20 09:46:57 -0500

    Creación de pantalla para ver el proyecto

---
+++ src/modules/projects/views/ProjectView.vue
@@ -0,0 +1,20 @@
+<template>
+  <BreadCrumbs :name="project?.name ?? 'No name'" />
+</template>
+
+<script lang="ts" setup>
+import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
+import { useProjectsStore } from '../store/projects.store';
+import { computed } from 'vue';
+
+interface Props {
+  id: string;
+}
+
+const props = defineProps<Props>();
+const projectStore = useProjectsStore();
+
+const project = computed(() =>
+  projectStore.projectList.find((project) => project?.id === props.id),
+);
+</script>
