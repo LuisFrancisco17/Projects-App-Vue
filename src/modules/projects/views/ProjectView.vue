c6c6dff8327aa3247210b149b8604bf8edff53b2
Luis Gil Florian <luisfrancisco17msn@gmail.com>
2025-04-20 20:35:19 -0500

    Se creó la función para agregar tareas

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,20 +1,80 @@
 <template>
-  <BreadCrumbs :name="project?.name ?? 'No name'" />
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'" />
+    </section>
+
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="task in project?.tasks" :key="task.id" class="hover">
+              <th>2</th>
+              <td>{{ task.name }}</td>
+              <td>Dektop Support Technician</td>
+            </tr>
+            <tr class="hover">
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                  v-model="newTask"
+                  @keyup.enter="addTask"
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../store/projects.store';
-import { computed } from 'vue';
+import type { Project } from '../interfaces/project.interface';
 
 interface Props {
   id: string;
 }
-
+const router = useRouter();
 const props = defineProps<Props>();
 const projectStore = useProjectsStore();
+const project = ref<Project | null>();
+const newTask = ref('');
 
-const project = computed(() =>
-  projectStore.projectList.find((project) => project?.id === props.id),
+// let project = projectStore.projectList.find((project) => project.id === props.id);
+const addTask = () => {
+  if (!project.value) return;
+  projectStore.addTaskToProject(project.value.id, newTask.value);
+  newTask.value = '';
+};
+
+watch(
+  () => props.id,
+  () => {
+    project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
+  },
+  {
+    immediate: true,
+  },
 );
 </script>
